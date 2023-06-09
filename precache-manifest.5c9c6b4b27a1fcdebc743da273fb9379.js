2ad6d4d59e2c4b7b8653841f77549d098383b46e
Abdullah Acar <aacar947@gmail.com>
2023-06-09 14:53:27 +0300

    Updates

---
+++ precache-manifest.5c9c6b4b27a1fcdebc743da273fb9379.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe993170d447ec7d94a81271eecd20fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2583a308682b59efc769",
+    "url": "./static/css/main.b0bfa5cf.chunk.css"
+  },
+  {
+    "revision": "ef97fce50502abf11e05",
+    "url": "./static/js/2.f649b257.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f649b257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2583a308682b59efc769",
+    "url": "./static/js/main.d06247d9.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.d06247d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e14bb03d7fca70ded93",
+    "url": "./static/js/runtime-main.ef1b9943.js"
+  }
+]);
